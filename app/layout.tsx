963beb134e34a4a609476d224c03fd3e739571f2
johnnyaustincasper <johnnyaustincasper@gmail.com>
2026-03-16 06:49:26 -0500

    solid #bfdbfe blue background, no gradient shift

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 }
 
 export const viewport: Viewport = {
-  themeColor: '#fecdd3',
+  themeColor: '#bfdbfe',
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
